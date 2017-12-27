0c7986726acc1c9102345fc4895892f33c16a67e
Ranga Naik Gugulothu <rangasmile741@gmail.com>
2017-12-26 19:00:40 -0500

    My project setup complted successfully

---
+++ shoppingbackend/src/main/java/net/grn/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.grn.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
